e516d04251f0aef3df6d0e7addfc48175b087b16
selvakisar <130025654+selvakisar@users.noreply.github.com>
2024-03-24 17:52:17 +0530

    bug fix

--- src/modules/Form/index.js
+++ src/modules/Form/index.js
@@ -33,7 +33,7 @@ const Form = ({
             if(resData.token) {
                 localStorage.setItem('user:token', resData.token)
                 localStorage.setItem('user:detail', JSON.stringify(resData.user))
-                navigate('/login')
+                navigate('/')
             }
         }
     }
